29f13f48e7a4577727116d553713f9aa010b3dea
guanshanyou <gsy940419@163com>
2020-11-13 13:02:36 +0800

    add a new Function

--- Algorithm/list.py
+++ Algorithm/list.py
@@ -1,16 +1,22 @@
-def is_palindrome(List):
-    list = List[::-1]
-    if list == List:
+
+
+def is_palindrome(input_list):
+    list = input_list[::-1]
+    if list == input_list:
         print("This List are  palindrome")
     else:
         print("This List are not palindrome")
-def is_palindrome1(List):
-    list = List
+
+
+def is_palindrome1(input_list):
+    list = input_list
     list.reverse()
-    if List == list:
+    if input_list == list:
         print("This List are  palindrome")
     else:
         print("This List are not palindrome")
-List = [1, 2, 3, 2, 1]       
+
+
+List = [1, 2, 3, 2, 1]
 is_palindrome(List)
 is_palindrome1(List)
